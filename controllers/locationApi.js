98bd954b9318699833d7213720f0df45e20d9514
Re-han <mamateen48@gmail.com>
2021-05-16 14:06:58 +0530

    location crud operation completed

--- controllers/locationApi.js
+++ controllers/locationApi.js
@@ -1,29 +1,117 @@
 const userPermission = require("../config/middlewares/authorization");
-const locationService = require("../services/locationService"); 
-const  constants = require("../config/constants");
+const locationService = require("../services/locationService");
+const constants = require("../config/constants");
 
-module.exports = function(express){
-    let api  = express.Router();
-    //api.use(userPermission.isValidUser);
-    api.post("/createcountry",async (req,res)=>{
-        try{
-            //console.log(req.body);
-            let data =  await locationService.createCountry(req.body);            
-            res.json({statusCode:constants.STATUS_200,data:data})
-        }catch(e){            
-            return userPermission.generateError(constants.STATUS_500,e)
-        }
-    });
-    api.post("/createstates",async (req,res)=>{
-        try{
-            //console.log(req.body);
-            let data =  await locationService.createState(req.body);            
-            res.json({statusCode:constants.STATUS_200,data:data})
-        }catch(e){            
-            return userPermission.generateError(constants.STATUS_500,e)
-        }
-    });
+module.exports = function (express) {
+  let api = express.Router();
+  //api.use(userPermission.isValidUser);
+  api.post("/createcountry", async (req, res) => {
+    try {
+      //console.log(req.body);
+      let data = await locationService.createCountry(req.body);
+      res.json({ statusCode: constants.STATUS_200, data: data });
+    } catch (e) {
+      return userPermission.generateError(constants.STATUS_500, e);
+    }
+  });
+  api.get("/getcountries", async (req, res) => {
+    try {
+      let data = await locationService.fetchCountries();
+      res.json({ statusCode: constants.STATUS_200, data: data });
+    } catch (e) {
+      return userPermission.generateError(constants.STATUS_500, e);
+    }
+  });
 
+  api.post("/updatecountry/:id", async (req, res) => {
+    try {
+      //console.log(req.body);
+      let data = await locationService.updateCountry(req.params.id, req.body);
+      res.json({
+        statusCode: constants.STATUS_200,
+        message: constants.STATUS_MSG_Update,
+      });
+    } catch (e) {
+      return userPermission.generateError(constants.STATUS_500, e);
+    }
+  });
 
-    return api;
-}
+  api.get("/getcountry/:id", async (req, res) => {
+    try {
+      let data = await locationService.getCountry(req.params.id);
+      res.json({
+        statusCode: constants.STATUS_200,
+        data: data,
+      });
+    } catch (e) {
+      return userPermission.generateError(constants.STATUS_500, e);
+    }
+  });
+
+  api.get("/deletecountry/:id", async (req, res) => {
+    try {
+      let data = await locationService.deleteCountry(req.params.id);
+      res.json({
+        statusCode: constants.STATUS_200,
+        message: constants.STATUS_MSG_Delete,
+      });
+    } catch (e) {
+      return userPermission.generateError(constants.STATUS_500, e);
+    }
+  });
+  api.post("/createstates", async (req, res) => {
+    try {
+      //console.log(req.body);
+      let data = await locationService.createState(req.body);
+      res.json({ statusCode: constants.STATUS_200, data: data });
+    } catch (e) {
+      return userPermission.generateError(constants.STATUS_500, e);
+    }
+  });
+  api.get("/getstates", async (req, res) => {
+    try {
+      let data = await locationService.fetchStates();
+      res.json({ statusCode: constants.STATUS_200, data: data });
+    } catch (e) {
+      return userPermission.generateError(constants.STATUS_500, e);
+    }
+  });
+  api.post("/updatestate/:id", async (req, res) => {
+    try {
+      //console.log(req.body);
+      let data = await locationService.updateState(req.params.id, req.body);
+      res.json({
+        statusCode: constants.STATUS_200,
+        message: constants.STATUS_MSG_Update,
+      });
+    } catch (e) {
+      return userPermission.generateError(constants.STATUS_500, e);
+    }
+  });
+
+  api.get("/getstate/:id", async (req, res) => {
+    try {
+      let data = await locationService.getState(req.params.id);
+      res.json({
+        statusCode: constants.STATUS_200,
+        data: data,
+      });
+    } catch (e) {
+      return userPermission.generateError(constants.STATUS_500, e);
+    }
+  });
+
+  api.get("/deletestate/:id", async (req, res) => {
+    try {
+      let data = await locationService.deleteState(req.params.id);
+      res.json({
+        statusCode: constants.STATUS_200,
+        message: constants.STATUS_MSG_Delete,
+      });
+    } catch (e) {
+      return userPermission.generateError(constants.STATUS_500, e);
+    }
+  });
+
+  return api;
+};
--- services/locationService.js
+++ services/locationService.js
@@ -1,28 +1,121 @@
-const db = require("../models")
+const db = require("../models");
 class LocationService {
-    constructor(){
-        this.db =  db;
+  constructor() {
+    this.db = db;
+  }
+  async createCountry(data) {
+    try {
+      //console.log(data,"-------");
+      let countryCreation = await this.db.Countries.create(data);
+      return countryCreation;
+    } catch (e) {
+      //console.error("error",e)
+      return e.message;
     }
-    async createCountry(data){
-        try{
-            //console.log(data,"-------");
-            let countryCreation = await this.db.countries.create(data);                       
-            return countryCreation;
-        }catch(e){
-            //console.error("error",e)
-            return e.message;
-        }
-    };
-    async createState(data){
-        try{
-            //console.log(data,"-------");
-            let satateCreation = await this.db.states.create(data);                       
-            return satateCreation;
-        }catch(e){
-            //console.error("error",e)
-            return e.message;
-        }
+  }
+  async fetchCountries() {
+    try {
+      //console.log(data,"-------");
+      let fetchCountries = await this.db.Countries.find({}).sort({
+        createDate: -1,
+      });
+      return fetchCountries;
+    } catch (e) {
+      //console.error("error",e)
+      return e.message;
     }
+  }
+  async updateCountry(id, data) {
+    try {
+      //console.log(data,"-------");
+      let updateCountry = await this.db.Countries.findOneAndUpdate(
+        { _id: id },
+        { $set: data },
+        { new: true }
+      );
+      return updateCountry;
+    } catch (e) {
+      //console.error("error",e)
+      return e.message;
+    }
+  }
+
+  async getCountry(id) {
+    try {
+      //console.log(data,"-------");
+      let getCountry = await this.db.Countries.findOne({ _id: id });
+      return getCountry;
+    } catch (e) {
+      //console.error("error",e)
+      return e.message;
+    }
+  }
+
+  async deleteCountry(id) {
+    try {
+      //console.log(data,"-------");
+      let deletedCountry = await this.db.Countries.deleteOne({ _id: id });
+      return deletedCountry;
+    } catch (e) {
+      //console.error("error",e)
+      return e.message;
+    }
+  }
+  async createState(data) {
+    try {
+      //console.log(data,"-------");
+      let stateCreation = await this.db.States.create(data);
+      return stateCreation;
+    } catch (e) {
+      //console.error("error",e)
+      return e.message;
+    }
+  }
+  async fetchStates() {
+    try {
+      //console.log(data,"-------");
+      let fetchStates = await this.db.States.find({}).sort({ createDate: -1 });
+      return fetchStates;
+    } catch (e) {
+      //console.error("error",e)
+      return e.message;
+    }
+  }
+  async getState(id) {
+    try {
+      //console.log(data,"-------");
+      let getState = await this.db.States.findOne({ _id: id });
+      return getState;
+    } catch (e) {
+      //console.error("error",e)
+      return e.message;
+    }
+  }
+
+  async deleteState(id) {
+    try {
+      //console.log(data,"-------");
+      let deletedState = await this.db.States.deleteOne({ _id: id });
+      return deletedState;
+    } catch (e) {
+      //console.error("error",e)
+      return e.message;
+    }
+  }
+  async updateState(id, data) {
+    try {
+      //console.log(data,"-------");
+      let updateState = await this.db.States.findOneAndUpdate(
+        { _id: id },
+        { $set: data },
+        { new: true }
+      );
+      return updateState;
+    } catch (e) {
+      //console.error("error",e)
+      return e.message;
+    }
+  }
 }
 
 module.exports = new LocationService();

7da71fcc92ddc62bd13908c3957016bcba1dc63a
nikhiltech91 <nikhil.tech91@gmail.com>
2021-09-07 17:26:17 +0530

    issue fixed

--- utils/event-emitter.js
+++ utils/event-emitter.js
@@ -11,29 +11,29 @@ class Events {
         this.plansService = plansService;
         this.notificationService = NotificationService;
         this.intializeListeners();
-        
+
     }
-    intializeListeners() {        
-        this.em.on(this.eventNames.Assign_Plan_To_User, async(data)=>{
-        let planDetails = await this.plansService.getFreePlan();
-        planDetails = JSON.parse(JSON.stringify(planDetails));   
-        let obj = {};     
-        obj["userId"] = data.userId;
-        obj["planId"] = planDetails.data._id;  
-        obj["planType"] = planDetails.data.planType; 
-        delete planDetails.data._id;
-        await this.plansService.createActivePlan(obj);
-        await this.plansService.createPlanHistory(obj);
-      });
-      this.em.on(this.eventNames.GENERATE_NOTIFICATION, async(data)=>{
-        let notifyObj = await this.getNotificationObject(data);
-        await this.notificationService.saveNotifications(notifyObj);
-      })
-    } 
-    
-    async getNotificationObject(data){
+    intializeListeners() {
+        this.em.on(this.eventNames.Assign_Plan_To_User, async (data) => {
+            let planDetails = await this.plansService.getFreePlan();
+            planDetails = JSON.parse(JSON.stringify(planDetails));
+            let obj = {};
+            obj["userId"] = data.userId;
+            obj["planId"] = planDetails.data._id;
+            obj["planType"] = planDetails.data.planType;
+            delete planDetails.data._id;
+            await this.plansService.createActivePlan(obj);
+            await this.plansService.createPlanHistory(obj);
+        });
+        this.em.on(this.eventNames.GENERATE_NOTIFICATION, async (data) => {
+            let notifyObj = await this.getNotificationObject(data);
+            await this.notificationService.saveNotifications(notifyObj);
+        })
+    }
+
+    async getNotificationObject(data) {
         let obj = {};
-         switch(data["entity_type"]){
+        switch (data["entity_type"]) {
             case "Request":
                 obj["senderId"] = data["followedBy"];
                 obj["receiverId"] = data["userId"];
@@ -41,9 +41,9 @@ class Events {
                 obj["senderName"] = userInfo.fullName || null;
                 obj["senderImage"] = userInfo.profileUrl || null;
                 obj["content"] = "Friend request on your wall";
-                obj["entity_type"] =  data["entity_type"];
-                obj["entityId"] = data["_id"];   
-            break;
+                obj["entity_type"] = data["entity_type"];
+                obj["entityId"] = data["_id"];
+                break;
             case "Accept":
                 obj["senderId"] = data["userId"];
                 obj["receiverId"] = data["followedBy"];
@@ -51,43 +51,55 @@ class Events {
                 obj["senderName"] = userInfoStatus.fullName || null;
                 obj["senderImage"] = userInfoStatus.profileUrl || null;
                 obj["content"] = "Your Request Has been Accepted";
-                obj["entity_type"] =  data["entity_type"];
+                obj["entity_type"] = data["entity_type"];
                 obj["entityId"] = data["_id"];
-            break; 
+                break;
             case "Comment":
                 obj["senderId"] = data["userId"];
                 const receiverInfo = await getReveiverUserIdByPostId(data["postId"]);
                 const userInfoComment = await getUserNameAndImage(data["userId"]);
-                obj["receiverId"] = receiverInfo.userId;               
+                obj["receiverId"] = receiverInfo.userId;
                 obj["senderName"] = userInfoComment.fullName || null;
                 obj["senderImage"] = userInfoComment.profileUrl || null;
                 obj["content"] = "Commnted on your post";
-                obj["entity_type"] =  data["entity_type"];
-                obj["entityId"] = data["postId"]; 
+                obj["entity_type"] = data["entity_type"];
+                obj["entityId"] = data["postId"];
                 obj["comment"] = data["comment"];
-            break;
+                break;
             case "Like":
-                obj["senderId"] = data["userId"];                
+                obj["senderId"] = data["userId"];
                 const userInfoLike = await getUserNameAndImage(data["userId"]);
-                obj["receiverId"] = data["recieverId"];           
+                console.log(userInfoLike);
+                obj["receiverId"] = data["recieverId"];
                 obj["senderName"] = userInfoLike.fullName || null;
                 obj["senderImage"] = userInfoLike.profileUrl || null;
                 obj["content"] = "Commnted on your post";
-                obj["entity_type"] =  data["entity_type"];
+                obj["entity_type"] = data["entity_type"];
                 obj["entityId"] = data["postId"];
-            break;            
-        }   
+                break;
+        }
         return obj;
     }
 
-    async getUserNameAndImage(userId){
-        return await this.db.User({_id:userId},{fullName:1, profileUrl:1});
-    } 
-    async getReveiverUserIdByPostId(postId){
-        return await this.db.UserPosts({_id:postId},{userId:1});
+    async getUserNameAndImage(userId) {
+        try {
+            return await this.db.User({ _id: userId }, { fullName: 1, profileUrl: 1 });
+        } catch (err) {
+            console.log(err);
+            return err;
+        }
+
+    }
+    async getReveiverUserIdByPostId(postId) {
+        try {
+            return await this.db.UserPosts({ _id: postId }, { userId: 1 });
+        } catch (err) {
+            console.log(err);
+            return err;
+        }
     }
 
-    
+
 }
 let em = new Events().em;
 module.exports = em;
